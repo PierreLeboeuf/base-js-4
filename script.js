21ecad3f9cfd78ffa56800601e152086b765bac1
PierreLeboeuf <suckstone@gmail.com>
2025-05-20 15:48:14 +0200

    exo3

---
+++ script.js
@@ -0,0 +1,27 @@
+// Exercice 1
+
+const nombres = [1, 2, 3, 4, 5];
+
+for (let chiffre of nombres){
+console.log(chiffre);
+}
+
+// Exercice 2
+
+// function sommeTableau(tableau) {
+//   let somme = 0;
+//   for (let i = 0; i < tableau.length; i++) {
+//     somme += tableau[i];
+//   }
+//   return somme;
+// }
+
+// Exercice 3
+
+const mots = ["bonjour", "salut", "coucou"]
+
+console.log(mots.length);
+
+// for (let motPlusLong of mots){
+// console.log(motPlusLong);
+// }
