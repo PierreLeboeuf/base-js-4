6bb72cd84b6cf9e3be61e13556d421e10620d351
PierreLeboeuf <suckstone@gmail.com>
2025-05-21 16:55:26 +0200

    exo8

--- script.js
+++ script.js
@@ -9,18 +9,18 @@ for (let chiffre of nombres) {
 // // ------------------------------------------------
 // Exercice 2 - Calculer la somme des éléments d'un tableau
 // // ------------------------------------------------
-const tableau = [1, 2, 3, 4, 5]
-function sommeTableau(tableau) {
-    let somme = 0;
-    for (let i = 0; i <= tableau.length; i++) {
-        somme += i;
+// const tableau = [1, 2, 3, 4, 5]
+// function sommeTableau(tableau) {
+//     let somme = 0;
+//     for (let i = 0; i <= tableau.length; i++) {
+//         somme += i;
 
-    }
-    return somme;
+//     }
+//     return somme;
 
-};
+// };
 
-console.log(sommeTableau(tableau));
+// console.log(sommeTableau(tableau));
 
 // // ------------------------------------------------
 // Exercice 3 - Trouver le mot le plus long dans un tableau
@@ -71,18 +71,110 @@ console.log(motPlusLong(tableauMots));
 // // ------------------------------------------------
 // Exercice 5 - Compter le nombre de voyelles dans une chaine
 // // ------------------------------------------------
-let chaine = "ordi" ;
+let chaine = "bonjour";
+// Méthode avec for simple
+// function compterVoyelles(chaine){
+//     let nbVoyelles = 0 ;
+//     for (let i = 0; i < chaine.length; i++) {
+//         let voyelles = chaine[i];
+//         if ((voyelles === "a") || (voyelles === "e") || (voyelles === "i") || (voyelles === "o") || (voyelles === "u") || (voyelles === "y")){
+//             nbVoyelles++;
+//         }
+//     }
+//     return nbVoyelles
+// }
 
-function compterVoyelles(chaine){
-    let nbVoyelles = 0 ;
-    for (let i = 0; i < chaine.length; i++) {
-        let voyelles = chaine[i];
-        if ((voyelles === "a") || (voyelles === "e") || (voyelles === "i") || (voyelles === "o") || (voyelles === "u") || (voyelles === "y")){
+// Méthode avec boucle dans un tableau
+
+function compterVoyelles(chaine) {
+    let nbVoyelles = 0;
+    let voyelles = ['a', 'e', 'i', 'o', 'u', 'y'];
+    for (let lettre of chaine) {
+        if (voyelles.includes(lettre)) { // si une voyelle du tableau est inclus dans le mot alors incrémenter nbVoyelles
             nbVoyelles++;
         }
-        
     }
     return nbVoyelles
 }
 
 console.log(compterVoyelles(chaine));
+
+// // ------------------------------------------------
+// Exercice 6 - Filtrer les nombres pairs d'un tableau
+// // ------------------------------------------------
+const tableau = []
+const tableauOrigine = [1, 2, 3, 4, 5, 6]
+
+function nombresPairs(tableau) {
+    let pairs;
+    for (let pairs of tableauOrigine) {
+        if (pairs % 2 === 0) {
+            tableau.push(pairs);
+        }
+    }
+    return pairs
+}
+
+console.log(nombresPairs(tableau));
+console.log(tableau);
+
+
+// // ------------------------------------------------
+// Exercice 7 - Compter les propriétés d'un objet
+// // ------------------------------------------------
+const objet = {
+    prenom: "Luc",
+    age: 30,
+    profession: "developpeur"
+};
+// avec une boucle
+// function compterProprietes(objet) {
+//     let nbObjet = 0;
+//     for (let clé in objet) {
+//         if (objet.hasOwnProperty(clé))
+//             nbObjet++;
+//     }
+//     return nbObjet;
+// }
+
+// console.log(compterProprietes(objet));
+
+// avec fonction javascrip
+
+function compterProprietes(objet) {
+    return Object.keys(objet).length;
+}
+console.log(compterProprietes(objet));
+
+// // ------------------------------------------------
+// Exercice 8 : Convertir un tableau d'objets en un tableau de valeurs
+// // ------------------------------------------------
+
+let produits = [
+    { nom: "Stylo", prix: 2 },
+    { nom: "Cahier", prix: 5 },
+    { nom: "Gomme", prix: 1 }
+];
+
+let nomProduits = [];
+
+// function extraireNoms(produits) {
+// }
+
+for (let produit of produits) {
+    console.log(`${produit.nom}`);
+
+    nomProduits.push(produit.nom);
+    console.log(nomProduits);
+}
+
+
+// // ------------------------------------------------
+// Exercice 9 : Calculer la factorielle d'un nombre
+// // ------------------------------------------------
+
+
+
+// // ------------------------------------------------
+// Exercice 10 : Vérifier si un tableau est trié en ordre croissant
+// // ------------------------------------------------
